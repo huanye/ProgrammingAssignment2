dc82be600c1ff3376d184c3b1fe6c1d22ef9659e
huanye <huanyeliu@gmail.com>
2014-08-10 11:15:28 +0800

    this is the R file for PS2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix returns a list which represents a special matrix object that can cache its inverse 
+## cacheSolve return the inverse matrix of the special matrix returned by function makeCacheMatrix
 
-## Write a short comment describing this function
+## The returned list represents a matrix with "memory" capacity-its cached inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	  inverse = NULL
+	  set<-function(y){
+	  	 x<<-y
+	  	 m<<-NULL
+	   }
+	   get<-function() x
+	   setinverse<- function(matrix) m<<-matrix
+	   getinverse<- function() m 
+      list(set =set,get=get,setinverse = setinverse,getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse matrix of x by first checking the cache value of x
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        a = x$getinverse()
+        if (!is.null(a)){
+        	    message("get cached data")
+        	    return (a)
+        } 
+        data= x$get()
+        inversed = solve(data,...)
+        x$setinverse(inversed)
+		inversed
 }
